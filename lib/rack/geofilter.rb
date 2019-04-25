4bc817986c9901fa263855cd3a7e54f911fdff5d
Rob Kitson <rkitson@evidation.com>
2019-04-25 11:03:02 -0700

    Slight refactor with some renaming and the removal of side-effect from a setup method

--- lib/rack/geofilter.rb
+++ lib/rack/geofilter.rb
@@ -1,35 +1,40 @@
 module Rack
   class Geofilter
 
-  BLOCKED_COUNTRIES_KEY = 'BLOCKED_COUNTRIES'
-  CLOUDFLARE_IPCOUNTRY_KEY = 'HTTP_CF_IPCOUNTRY'
+  BLOCKED_COUNTRIES_KEY = 'BLOCKED_COUNTRIES'.freeze
+  CLOUDFLARE_IPCOUNTRY_KEY = 'HTTP_CF_IPCOUNTRY'.freeze
 
   def initialize app, country_string=nil
     @app = app
-    setup_filter(ENV[BLOCKED_COUNTRIES_KEY] || country_string)
+
+    setup_blocked_countries(ENV[BLOCKED_COUNTRIES_KEY] || country_string)
+    @filter_enabled = @blocked_countries.count > 0
   end
 
-  def call env
-    if should_block?(env)
+  def call request
+    if should_block?(request)
       return error_message
     end
 
-    @app.call(env)
+    @app.call(request)
   end
 
   private
 
-  def setup_filter country_string
+  def setup_blocked_countries country_string
+
     if !country_string.nil? && country_string != ""
-      @blocked_countries = country_string.split(',').map(&:strip).uniq.compact
-      @filter_enabled = @blocked_countries.count > 0
+      @blocked_countries = Hash[country_string.downcase.split(',').map(&:strip).uniq.compact.collect{|c| [c, true]} ].freeze
+    else
+      @blocked_countries = {}.freeze
     end
   end
 
-  def should_block?(env)
-    return unless @filter_enabled
+  def should_block?(request)
+    return false unless @filter_enabled
+    return false unless request[CLOUDFLARE_IPCOUNTRY_KEY]
 
-    @blocked_countries.include? env[CLOUDFLARE_IPCOUNTRY_KEY]
+    @blocked_countries.key?(request[CLOUDFLARE_IPCOUNTRY_KEY].strip.downcase)
   end
 
   def error_message
